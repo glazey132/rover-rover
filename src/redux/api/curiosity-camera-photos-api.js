3b76c30cba082f4dd52a5ced4c4958bde92e5e40
glazey132 <alglaze132@gmail.com>
2019-06-08 23:47:08 -0700

    cleanup logs

--- src/redux/api/curiosity-camera-photos-api.js
+++ src/redux/api/curiosity-camera-photos-api.js
@@ -2,12 +2,9 @@ export const fetchCuriosityCameraPhotos = async (cameraType, date) => {
     try {
         //clicking camera button sends request. only date selection should doo that
         // change check for date here to not check for date and enforce searching with date as a needed param
-        console.log('the cameratype in cury camera photos =: ', cameraType, date)
         const response = cameraType === 'ALL' ? await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${process.env.REACT_APP_NASA_API_KEY}`)
                                         : await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${cameraType}&api_key=${process.env.REACT_APP_NASA_API_KEY}`);
-        console.log(' the resp froom fetch curi => ', response);
         const data = await response.json();
-        console.log('the data afee resp => ', data)
         return data;
     } catch (error) {
         console.log('error fetching curiosity camera photos => ', error)
--- src/redux/sagas/curiosity-camera-photos.js
+++ src/redux/sagas/curiosity-camera-photos.js
@@ -6,7 +6,6 @@ import cameraUrlMap from '../../assets/camera-url-values';
 import { getRover, getDate, getCamera} from './selectors'
 
 function* getCameraPhotos(action) {
-    console.log('actioin in curi cam saga > ', action)
     const cameraUrl = cameraUrlMap.curiosity[`${action.payload.camera}`]
     try {
         const data = yield call(fetchCuriosityCameraPhotos, cameraUrl, action.payload.date)
